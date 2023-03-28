3bf3c256a930a75a1840fb5bfa4904542da9403d
Cl3arglass <haltaffer@gmail.com>
2023-03-28 15:32:17 -0700

    and again

--- pages/responses/six.js
+++ pages/responses/six.js
@@ -1,5 +1,5 @@
 export default function Six() {
-    return <div> I suspect that the most common problem customers run into on Vercel is why their git commits didn't trigger a deployment. As a CSE at Vercel I would make sure to share the relevant guide and make sure my team mates know how to debug by the commit SHA that customers will submit.
+    return <div> I suspect that the most common problem customers run into on Vercel is why their git commits did not trigger a deployment. As a CSE at Vercel I would make sure to share the relevant guide and make sure my team mates know how to debug by the commit SHA that customers will submit.
     </div>
   }
 
